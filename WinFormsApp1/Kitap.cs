08f646ebfc346ab4c79de3f501b02195e3293176
Selim <selimdemi.0@gmail.com>
2024-03-29 22:40:33 +0300

    Kitap sinifi eklendi

---
+++ WinFormsApp1/Kitap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Kitap
+    {
+
+        public string Kitapismi { get; set; }
+        public string Yazari {  get; set; }
+        public int sayfaSayisi {  get; set; }
+        public int SeriNo {  get; set; }
+
+        public Kitap() { }
+        public void tabloEkle(DataTable dt)
+        {
+            dt.Rows.Add(new Object[] { Kitapismi, Yazari, sayfaSayisi, SeriNo });
+        }
+    }
+}
