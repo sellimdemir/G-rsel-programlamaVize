0ce94886c974435abe1ebcf8e5bcb3f5f13a82c9
sellimdemir <selimdemi.0@gmail.com>
2024-04-13 23:49:11 +0300

    Görev-5

--- WinFormsApp1/Form2.Designer.cs
+++ WinFormsApp1/Form2.Designer.cs
@@ -43,109 +43,118 @@
             butonEkleUye = new Button();
             butonGuncelleUye = new Button();
             butonSilUye = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(169, 28);
+            textBox1.Location = new Point(193, 37);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(243, 23);
+            textBox1.Size = new Size(277, 27);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(169, 251);
+            textBox2.Location = new Point(193, 335);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(243, 23);
+            textBox2.Size = new Size(277, 27);
             textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(169, 332);
+            textBox3.Location = new Point(193, 443);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(243, 23);
+            textBox3.Size = new Size(277, 27);
             textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(169, 173);
+            textBox4.Location = new Point(193, 231);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(243, 23);
+            textBox4.Size = new Size(277, 27);
             textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(169, 99);
+            textBox5.Location = new Point(193, 132);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(243, 23);
+            textBox5.Size = new Size(277, 27);
             textBox5.TabIndex = 4;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(41, 36);
+            label1.Location = new Point(47, 48);
             label1.Name = "label1";
-            label1.Size = new Size(45, 15);
+            label1.Size = new Size(57, 20);
             label1.TabIndex = 5;
             label1.Text = "üye adı";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(41, 423);
+            label2.Location = new Point(47, 564);
             label2.Name = "label2";
-            label2.Size = new Size(17, 15);
+            label2.Size = new Size(21, 20);
             label2.TabIndex = 6;
             label2.Text = "tc";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(41, 181);
+            label3.Location = new Point(47, 241);
             label3.Name = "label3";
-            label3.Size = new Size(76, 15);
+            label3.Size = new Size(95, 20);
             label3.TabIndex = 7;
             label3.Text = "doğum tarihi";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(41, 259);
+            label4.Location = new Point(47, 345);
             label4.Name = "label4";
-            label4.Size = new Size(50, 15);
+            label4.Size = new Size(62, 20);
             label4.TabIndex = 8;
             label4.Text = "cinsiyeti";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(41, 107);
+            label5.Location = new Point(47, 143);
             label5.Name = "label5";
-            label5.Size = new Size(63, 15);
+            label5.Size = new Size(79, 20);
             label5.TabIndex = 9;
             label5.Text = "üye soyadı";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(41, 340);
+            label6.Location = new Point(47, 453);
             label6.Name = "label6";
-            label6.Size = new Size(96, 15);
+            label6.Size = new Size(120, 20);
             label6.TabIndex = 10;
             label6.Text = "telefon numarası";
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(169, 415);
+            textBox6.Location = new Point(193, 553);
+            textBox6.Margin = new Padding(3, 4, 3, 4);
             textBox6.Name = "textBox6";
-            textBox6.Size = new Size(243, 23);
+            textBox6.Size = new Size(277, 27);
             textBox6.TabIndex = 11;
             // 
             // butonEkleUye
             // 
-            butonEkleUye.Location = new Point(514, 57);
+            butonEkleUye.Location = new Point(587, 76);
+            butonEkleUye.Margin = new Padding(3, 4, 3, 4);
             butonEkleUye.Name = "butonEkleUye";
-            butonEkleUye.Size = new Size(75, 23);
+            butonEkleUye.Size = new Size(86, 31);
             butonEkleUye.TabIndex = 12;
             butonEkleUye.Text = "ekle";
             butonEkleUye.UseVisualStyleBackColor = true;
@@ -153,28 +162,42 @@
             // 
             // butonGuncelleUye
             // 
-            butonGuncelleUye.Location = new Point(514, 121);
+            butonGuncelleUye.Location = new Point(587, 161);
+            butonGuncelleUye.Margin = new Padding(3, 4, 3, 4);
             butonGuncelleUye.Name = "butonGuncelleUye";
-            butonGuncelleUye.Size = new Size(75, 23);
+            butonGuncelleUye.Size = new Size(86, 31);
             butonGuncelleUye.TabIndex = 12;
             butonGuncelleUye.Text = "guncelle";
             butonGuncelleUye.UseVisualStyleBackColor = true;
+            butonGuncelleUye.Click += butonGuncelleUye_Click;
             // 
             // butonSilUye
             // 
-            butonSilUye.Location = new Point(514, 177);
+            butonSilUye.Location = new Point(587, 236);
+            butonSilUye.Margin = new Padding(3, 4, 3, 4);
             butonSilUye.Name = "butonSilUye";
-            butonSilUye.Size = new Size(75, 23);
+            butonSilUye.Size = new Size(86, 31);
             butonSilUye.TabIndex = 12;
             butonSilUye.Text = "sil";
             butonSilUye.UseVisualStyleBackColor = true;
+            butonSilUye.Click += butonSilUye_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(706, 14);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(571, 570);
+            dataGridView1.TabIndex = 13;
             // 
             // UyeEkle
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(800, 475);
+            ClientSize = new Size(1306, 633);
+            Controls.Add(dataGridView1);
             Controls.Add(butonSilUye);
             Controls.Add(butonGuncelleUye);
             Controls.Add(butonEkleUye);
@@ -190,9 +213,11 @@
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "UyeEkle";
             Text = "Form2";
             Load += Form2_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -214,5 +239,6 @@
         private Button butonEkleUye;
         private Button butonGuncelleUye;
         private Button butonSilUye;
+        private DataGridView dataGridView1;
     }
 }
--- WinFormsApp1/Form2.cs
+++ WinFormsApp1/Form2.cs
@@ -1,38 +1,126 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 using System.Windows.Forms;
 
 namespace WinFormsApp1
 {
     public partial class UyeEkle : Form
     {
+        private string databasePath = "database.db"; 
+
         public UyeEkle()
         {
             InitializeComponent();
+
+     
+            if (!File.Exists(databasePath))
+            {
+                SQLiteConnection.CreateFile(databasePath); 
+                CreateTable(); 
+            }
+
+            
+            Form2_Load(this, EventArgs.Empty);
+        }
+
+        private void CreateTable()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + databasePath + ";Version=3;"))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                {
+                    // Uyeler tablosunu oluştur
+                    cmd.CommandText = @"CREATE TABLE Uyeler (
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        Ad TEXT,
+                                        Soyad TEXT,
+                                        DogumTarihi TEXT,
+                                        Cinsiyet TEXT,
+                                        Telefon TEXT,
+                                        Tc TEXT UNIQUE)";
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            VerileriYukle();
+        }
 
+        private void VerileriYukle()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + databasePath + ";Version=3;"))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM Uyeler", conn))
+                {
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        dataGridView1.DataSource = dt;
+                    }
+                }
+            }
         }
 
         private void butonEkleUye_Click(object sender, EventArgs e)
         {
-            Uye uye=new Uye();
-            uye.Adi = textBox1.Text;
-            uye.Soyadi = textBox2.Text;
-            uye.dogumTarihi = Convert.ToInt32(textBox3.Text);
-            uye.Telefon=Convert.ToInt32(textBox4.Text);
-            uye.TC=Convert.ToInt32(textBox5.Text);
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + databasePath + ";Version=3;"))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                {
+                    cmd.CommandText = "INSERT INTO Uyeler (Ad, Soyad, DogumTarihi, Cinsiyet, Telefon, Tc) VALUES (@ad, @soyad, @dogumTarihi, @cinsiyet, @telefon, @tc)";
+                    cmd.Parameters.AddWithValue("@ad", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@soyad", textBox5.Text);
+                    cmd.Parameters.AddWithValue("@dogumTarihi", textBox4.Text);
+                    cmd.Parameters.AddWithValue("@cinsiyet", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@telefon", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@tc", textBox6.Text);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            VerileriYukle();
+        }
 
-            Uye.UyeList.Add(uye);
+        private void butonSilUye_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + databasePath + ";Version=3;"))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                {
+                    cmd.CommandText = "DELETE FROM Uyeler WHERE Tc = @tc";
+                    cmd.Parameters.AddWithValue("@tc", textBox6.Text);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            VerileriYukle();
+        }
 
+        private void butonGuncelleUye_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + databasePath + ";Version=3;"))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                {
+                    cmd.CommandText = "UPDATE Uyeler SET Ad = @ad, Soyad = @soyad, DogumTarihi = @dogumTarihi, Cinsiyet = @cinsiyet, Telefon = @telefon WHERE Tc = @tc";
+                    cmd.Parameters.AddWithValue("@ad", textBox1.Text);
+                    cmd.Parameters.AddWithValue("@soyad", textBox5.Text);
+                    cmd.Parameters.AddWithValue("@dogumTarihi", textBox4.Text);
+                    cmd.Parameters.AddWithValue("@cinsiyet", textBox2.Text);
+                    cmd.Parameters.AddWithValue("@telefon", textBox3.Text);
+                    cmd.Parameters.AddWithValue("@tc", textBox6.Text);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            VerileriYukle();
         }
     }
 }
